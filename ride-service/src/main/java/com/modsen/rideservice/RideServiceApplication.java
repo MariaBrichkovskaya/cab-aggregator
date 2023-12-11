1c2d0944663baf819d6c6a528d6354a974f64d98
MariaBrichkovskaya <brichkovskayam@gmail.com>
2023-12-11 21:32:39 +0300

    minor refactoring

--- ride-service/src/main/java/com/modsen/rideservice/RideServiceApplication.java
+++ ride-service/src/main/java/com/modsen/rideservice/RideServiceApplication.java
@@ -12,8 +12,4 @@ public class RideServiceApplication {
         SpringApplication.run(RideServiceApplication.class, args);
     }
 
-    @Bean
-    public ModelMapper modelMapper(){
-        return new ModelMapper();
-    }
 }
--- ride-service/src/main/java/com/modsen/rideservice/controller/RideController.java
+++ ride-service/src/main/java/com/modsen/rideservice/controller/RideController.java
@@ -2,6 +2,7 @@ package com.modsen.rideservice.controller;
 
 import com.modsen.rideservice.dto.request.RideRequest;
 import com.modsen.rideservice.dto.request.StatusRequest;
+import com.modsen.rideservice.dto.response.MessageResponse;
 import com.modsen.rideservice.dto.response.RideResponse;
 import com.modsen.rideservice.dto.response.RidesListResponse;
 import com.modsen.rideservice.service.RideService;
@@ -17,54 +18,58 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 public class RideController {
     private final RideService rideService;
+
     @GetMapping
     public ResponseEntity<RidesListResponse> getAll(@RequestParam(required = false, defaultValue = "1") int page,
                                                     @RequestParam(required = false, defaultValue = "10") int size,
-                                                    @RequestParam(name = "order_by", required = false) String orderBy){
-        RidesListResponse rides=rideService.findAll(page,size,orderBy);
+                                                    @RequestParam(name = "order_by", required = false) String orderBy) {
+        RidesListResponse rides = rideService.findAll(page, size, orderBy);
         return ResponseEntity.ok(rides);
     }
+
     @PostMapping
-    public ResponseEntity<String> createRide(@RequestBody @Valid RideRequest rideRequest) {
+    public ResponseEntity<MessageResponse> createRide(@RequestBody @Valid RideRequest rideRequest) {
         rideService.add(rideRequest);
-        return ResponseEntity.ok("Adding ride");
+        return ResponseEntity.ok(MessageResponse.builder().message("Adding ride").build());
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<String> deleteRide(@PathVariable Long id){
+    public ResponseEntity<MessageResponse> deleteRide(@PathVariable Long id) {
         rideService.delete(id);
-        return ResponseEntity.ok("Deleting ride with id " + id);
+        return ResponseEntity.ok(MessageResponse.builder().message("Deleting ride with id " + id).build());
     }
+
     @GetMapping("/{id}")
-    public ResponseEntity<RideResponse> rideInfo(@PathVariable Long id){
-        RideResponse ride=rideService.findById(id);
+    public ResponseEntity<RideResponse> rideInfo(@PathVariable Long id) {
+        RideResponse ride = rideService.findById(id);
         return ResponseEntity.ok(ride);
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<String> updateRide(@PathVariable Long id,@RequestBody @Valid RideRequest rideRequest)
-    {
-        rideService.update(rideRequest,id);
-        return ResponseEntity.ok("Editing ride with id " + id);
+    public ResponseEntity<MessageResponse> updateRide(@PathVariable Long id, @RequestBody @Valid RideRequest rideRequest) {
+        rideService.update(rideRequest, id);
+        return ResponseEntity.ok(MessageResponse.builder().message("Editing ride with id " + id).build());
     }
+
     @GetMapping("/passenger/{passenger_id}")
-    public ResponseEntity<RidesListResponse> passengerRides(@PathVariable(name = "passenger_id") long passengerId,@RequestParam(required = false, defaultValue = "1") int page,
-                                                    @RequestParam(required = false, defaultValue = "10") int size,
-                                                    @RequestParam(name = "order_by", required = false) String orderBy){
-        RidesListResponse rides=rideService.getRidesByPassengerId(passengerId,page,size,orderBy);
-        return ResponseEntity.ok(rides);
-    }
-    @GetMapping("/driver/{driver_id}")
-    public ResponseEntity<RidesListResponse> driverRides(@PathVariable(name = "driver_id") long driverId,@RequestParam(required = false, defaultValue = "1") int page,
+    public ResponseEntity<RidesListResponse> passengerRides(@PathVariable(name = "passenger_id") long passengerId, @RequestParam(required = false, defaultValue = "1") int page,
                                                             @RequestParam(required = false, defaultValue = "10") int size,
-                                                            @RequestParam(name = "order_by", required = false) String orderBy){
-        RidesListResponse rides=rideService.getRidesByDriverId(driverId,page,size,orderBy);
+                                                            @RequestParam(name = "order_by", required = false) String orderBy) {
+        RidesListResponse rides = rideService.getRidesByPassengerId(passengerId, page, size, orderBy);
         return ResponseEntity.ok(rides);
     }
+
+    @GetMapping("/driver/{driver_id}")
+    public ResponseEntity<RidesListResponse> driverRides(@PathVariable(name = "driver_id") long driverId, @RequestParam(required = false, defaultValue = "1") int page,
+                                                         @RequestParam(required = false, defaultValue = "10") int size,
+                                                         @RequestParam(name = "order_by", required = false) String orderBy) {
+        RidesListResponse rides = rideService.getRidesByDriverId(driverId, page, size, orderBy);
+        return ResponseEntity.ok(rides);
+    }
+
     @PutMapping("/status/{id}")
-    public ResponseEntity<String> editStatus(@PathVariable Long id,@RequestBody @Valid StatusRequest statusRequest)
-    {
-        rideService.editStatus(id,statusRequest);
-        return ResponseEntity.ok("Status updated to " + statusRequest.getStatus());
+    public ResponseEntity<MessageResponse> editStatus(@PathVariable Long id, @RequestBody @Valid StatusRequest statusRequest) {
+        rideService.editStatus(id, statusRequest);
+        return ResponseEntity.ok(MessageResponse.builder().message("Status updated to " + statusRequest.getStatus()).build());
     }
 }
