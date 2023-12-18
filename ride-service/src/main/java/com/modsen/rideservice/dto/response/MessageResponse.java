fa1ccf4ae1f2e98b2f20ede6afa5084725816102
MariaBrichkovskaya <brichkovskayam@gmail.com>
2023-12-18 11:26:59 +0300

    minor refactoring

--- ride-service/src/main/java/com/modsen/rideservice/dto/response/MessageResponse.java
+++ ride-service/src/main/java/com/modsen/rideservice/dto/response/MessageResponse.java
@@ -2,9 +2,11 @@ package com.modsen.rideservice.dto.response;
 
 import lombok.AccessLevel;
 import lombok.Builder;
+import lombok.Getter;
 import lombok.experimental.FieldDefaults;
 
 @Builder
+@Getter
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class MessageResponse {
     String message;
