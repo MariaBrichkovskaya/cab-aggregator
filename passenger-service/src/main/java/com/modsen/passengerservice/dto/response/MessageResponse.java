f67214810602fe4bc10c55ae162c9f93a2a21bd5
MariaBrichkovskaya <brichkovskayam@gmail.com>
2023-12-19 13:10:41 +0300

    minor refactoring

--- passenger-service/src/main/java/com/modsen/passengerservice/dto/response/MessageResponse.java
+++ passenger-service/src/main/java/com/modsen/passengerservice/dto/response/MessageResponse.java
@@ -2,10 +2,12 @@ package com.modsen.passengerservice.dto.response;
 
 import lombok.AccessLevel;
 import lombok.Builder;
+import lombok.Getter;
 import lombok.experimental.FieldDefaults;
 
 @Builder
 @FieldDefaults(level = AccessLevel.PRIVATE)
+@Getter
 public class MessageResponse {
     String message;
 }
